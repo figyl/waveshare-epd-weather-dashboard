4bb57cc440b4ec8100b4c8905e52c51085b7f850
mrbwburns <>
2023-12-12 00:17:06 +0100

    started adding mqtt interface for local sensors

---
+++ mqtt_client.py
@@ -0,0 +1,31 @@
+import json
+import paho.mqtt.client as mqtt
+
+
+class mqtt_client():
+    def __init__(self, host, port, user, password, topic):
+        self.topic = topic
+        self.state = 0
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.username_pw_set(user, password)
+        self.client.connect(host=host, port=port, keepalive=60)
+        self.client.loop_start()
+
+    def __exit__(self):
+        self.client.loop_stop()
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        self.client.subscribe(self.topic, 0)
+
+    # The callback for when a PUBLISH message is received from the server.
+    def on_message(self, client, userdata, msg):
+        # Decode the payload from bytes to a string
+        payload_string = str(msg.payload.decode("utf-8"))
+
+        # Parse the JSON string into a Python dictionary
+        self.state = json.loads(payload_string)
---
+++ room_temperature.py
@@ -0,0 +1,45 @@
+import os
+import json
+import time
+from mqtt_client import mqtt_client
+
+## Paths config
+_HERE = os.path.dirname(__file__)
+
+## Read Settings
+with open(os.path.join(_HERE, "config.json"), "r") as configfile:
+    config = json.load(configfile)
+mqtt_host = config["mqtt_host"]
+mqtt_port = config["mqtt_port"]
+mqtt_user = config["mqtt_user"]
+mqtt_pass = config["mqtt_pass"]
+mqtt_topic = config["mqtt_topic"]
+mqtt_temp_key = config["mqtt_temp_key"]
+mqtt_rH_key= config["mqtt_rH_key"]
+
+class mqtt_temperature(mqtt_client):
+    def __init__(self, host, port, user, password, topic):
+        super().__init__(host=host, port=port, user=user, password=password, topic=topic)
+        
+    def get_temperature(self):
+        if self.state != 0:
+            return self.state[mqtt_temp_key]
+        else: 
+            return None
+    
+    def get_rH(self):
+        if self.state != 0:
+            return self.state[mqtt_rH_key]
+        else: 
+            return None
+    
+
+def main():
+    my_temperature = mqtt_temperature(host=mqtt_host, port=mqtt_port, user=mqtt_user, password=mqtt_pass, topic=mqtt_topic)
+    while(True):
+        print(f"Temperatur: {my_temperature.get_temperature()}°C")
+        print(f"rF: {my_temperature.get_rH()}%")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
