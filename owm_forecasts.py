44c90aca377ff46097fe945a93679216ae326d2e
figyl <figyl@protonmail.com>
2023-12-26 23:22:16 +0100

    quick and dirty fix here

--- owm_forecasts.py
+++ owm_forecasts.py
@@ -135,6 +135,9 @@ def get_forecast_for_day(days_from_today: int, hourly_forecasts: list) -> dict:
         for f in hourly_forecasts
         if is_timestamp_within_range(timestamp=f["datetime"], start_time=start_time, end_time=end_time)
     ]
+    
+    if forecasts == []:
+        forecasts.append(hourly_forecasts[0])
 
     # Get rain and temperatures for that day
     temps = [f["temp"] for f in forecasts]
